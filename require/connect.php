30e9f220eee441f08891b18bad4406960802b33a
DevVali <valeriot789@gmail.com>
2022-07-10 19:47:31 +0200

    Create files for the require() funct

--- require/connect.php
+++ require/connect.php
@@ -5,6 +5,7 @@
 
 <script src="https://kit.fontawesome.com/ede9707ca2.js" crossorigin="anonymous"></script>
 
+<link rel="icon" href="https://cdn.discordapp.com/attachments/889423777123938315/995730047979757619/coin_favicon.png">
 <link rel="stylesheet" href="/styles.css">
 <link rel="stylesheet" href="https://devvali.github.io/Boostyle/index.css">
 
