58a005225c4805f7fdaae1e24d1f88159226c168
DevVali <valeriot789@gmail.com>
2022-07-10 13:09:40 +0200

    Create project files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="/styles.css">
+        <link rel="stylesheet" href="https://devvali.github.io/Boostyle/index.css">
+        <title>A JSON code template generator for Subway Surfers.</title>
+    </head>
+</html>
