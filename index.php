5a9a29a47e1ae7f4364aa5b1a7ddeaf84dc18014
DevVali <90271108+DevVali@users.noreply.github.com>
2022-07-11 15:25:50 +0000

    edit lines

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php require "/home/runner/subwaysurf/require/connect.php" ?>
-        <title>A JSON code template generator for Subway Surfers.</title>
-    </head>
-    <body>
-        <header>
-                <h1>A JSON code template generator for Subway Surfers.</h1>
-                <p style="color:#aaa;font-size:25px">Earn coins, keys and more for free in Subway Surfers.</p>
-                <img src="https://cdn.discordapp.com/attachments/889423777123938315/995664415410102272/subwaysurfers_01.png" style="width:600px;height:300px">
-        </header>
-        <h2>Android Tutorial</h2>
-        <ol style="display:grid;place-items: center;">
-            <li>Download <a href="https://play.google.com/store/apps/details?id=com.kiloo.subwaysurf&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">Subway Surfers</a>.</li>
-            <li>Open your file manager app.</li>
-            <li>Navigate between the folders.</li>
-            <li>Android → data → com.kiloo.subwaysurf → files → profile</li>
-            <li>Copy the generated code, find the file in the folder "profile" and paste it there.</li>
-        </ol>
-        <h2>Available generators</h2>
-        <a href="/generator/wallet.php"><button class="btn btn-primary" style="width:75%">wallet.json (coins, keys, ...)</button></a>
+  <head>
+<?php require "/home/runner/subwaysurf/require/connect.php" ?>
+    <title>A JSON code template generator for Subway Surfers.</title>
+  </head>
+  <body>
+    <header>
+      <h1>A JSON code template generator for Subway Surfers.</h1>
+      <p style="color:#aaa;font-size:25px">Earn coins, keys and more for free in Subway Surfers.</p>
+      <img src="https://cdn.discordapp.com/attachments/889423777123938315/995664415410102272/subwaysurfers_01.png" style="width:600px;height:300px">
+    </header>
+    <h2>Android Tutorial</h2>
+    <ol style="display:grid;place-items: center;">
+      <li>Download <a href="https://play.google.com/store/apps/details?id=com.kiloo.subwaysurf&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">Subway Surfers</a>.</li>
+      <li>Open your file manager app.</li>
+      <li>Navigate between the folders.</li>
+      <li>Android → data → com.kiloo.subwaysurf → files → profile</li>
+      <li>Copy the generated code, find the file in the folder "profile" and paste it there.</li>
+    </ol>
+    <h2>Available generators</h2>
+    <a href="/generator/wallet.php"><button class="btn btn-primary" style="width:75%">wallet.json (coins, keys, ...)</button></a>
 
-        <br><br><br>
-        <?php require "/home/runner/subwaysurf/require/footer.php" ?>
+    <br><br><br>
+<?php require "/home/runner/subwaysurf/require/footer.php" ?>
 </html>
