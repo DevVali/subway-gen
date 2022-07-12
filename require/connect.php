9f3036de0c10fb5a113077136de6f339daccf255
DevVali <90271108+DevVali@users.noreply.github.com>
2022-07-12 10:55:21 +0000

    Add meta tags

--- require/connect.php
+++ require/connect.php
@@ -3,6 +3,16 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<meta property="og:type" content="website"/>
+<meta name="theme-color" content="#5865f2"/>
+<meta property="og:site_name" content="© <?php echo date("Y"); ?> Vali, all rights reserved"/>
+<meta property="og:title" content="A JSON code template generator for Subway Surfers."/>
+<meta property="og:image" content="https://cdn.discordapp.com/attachments/889423777123938315/995730047979757619/coin_favicon.png"/>
+<meta property="og:url" content="https://subwaysurf.devvali.repl.co/"/>
+<meta property="twitter:card" content="summary_large_image"/>
+<meta name="author" content="Vali"/>
+<meta name="copyright" content="© <?php echo date("Y"); ?> Vali, all rights reserved"/>
+
 <script src="https://kit.fontawesome.com/ede9707ca2.js" crossorigin="anonymous"></script>
 
 <link rel="icon" href="https://cdn.discordapp.com/attachments/889423777123938315/995730047979757619/coin_favicon.png">
